d9bb9e1410378906cbd09207983027c593c4867a
kk <likang@hexfuture.net>
2022-05-19 20:53:49 +0800

    first

--- src/Modularity/System/JNPF.System.Entitys/Entity/Permission/ICU_BUNDLE_3H6H.cs
+++ src/Modularity/System/JNPF.System.Entitys/Entity/Permission/ICU_BUNDLE_3H6H.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using JNPF.Common.Const;
+using SqlSugar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace JNPF.System.Entitys.Entity.Permission
 {
+
+    /// <summary>
+    /// Bundle治疗3小时和6小时治疗项目总体记录数据
+    /// </summary>
+    [SugarTable("BASE_AUTHORIZE")]
+    [Tenant(ClaimConst.TENANT_ID)]
     public class ICU_BUNDLE_3H6H
     {
+        [SugarColumn(ColumnName = "UUID")]
+        public string UUID { get; set; }
     }
 }
